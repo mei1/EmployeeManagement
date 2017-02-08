c0e682c5351b6be257fbf51e3275b2692c7f380f
qimei <qimei@192.168.0.10>
2017-02-09 07:14:11 +0800

    insert methods for emp, idcard and workstation

--- src/org/qimei/training/db/EmployeeManagementDBHelper.java
+++ src/org/qimei/training/db/EmployeeManagementDBHelper.java
@@ -1,19 +1,42 @@
 package org.qimei.training.db;
 
+import java.sql.SQLException;
+
+import org.qimei.training.pojo.Employee;
+import org.qimei.training.pojo.IdCard;
+import org.qimei.training.pojo.WorkStation;
 
 public class EmployeeManagementDBHelper {
-	
+
 	private static EmployeeManagementDBHelper empMgmtDBHelper = null;
-	
+
 	private EmployeeManagementDBHelper() {
 		super();
 	}
-	
+
 	public static EmployeeManagementDBHelper getInstance() {
 		if (empMgmtDBHelper == null) {
 			empMgmtDBHelper = new EmployeeManagementDBHelper();
 		}
 		return empMgmtDBHelper;
+	}
+
+	public int insertIntoEmps(Employee emp) throws SQLException {
+		int rowCount = EmployeeManagementDBManager.getInstance().getEmpDao().create(emp);
+
+		return rowCount;
+	}
+
+	public int inserIntoIdCard(IdCard empIdCard) throws SQLException {
+		int rowCount = EmployeeManagementDBManager.getInstance().getIdCardDao().create(empIdCard);
+
+		return rowCount;
+	}
+
+	public int insetIntoWorkStation(WorkStation empWorkStation) throws SQLException {
+		int rowCount = EmployeeManagementDBManager.getInstance().getWorkStationDao().create(empWorkStation);
+
+		return rowCount;
 	}
 
 }
