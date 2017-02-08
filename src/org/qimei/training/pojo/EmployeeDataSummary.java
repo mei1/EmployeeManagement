f10ec2e2159c14d49c944e3711d097bb80f66541
qimei <qimei@192.168.0.10>
2017-02-08 22:57:23 +0800

    Create employee summary object for grouping

---
+++ src/org/qimei/training/pojo/EmployeeDataSummary.java
@@ -0,0 +1,33 @@
+package org.qimei.training.pojo;
+
+public class EmployeeDataSummary {
+
+	private Employee emp;
+	private IdCard empIdCard;
+	private WorkStation empWorkStation;
+
+	public Employee getEmp() {
+		return emp;
+	}
+
+	public void setEmp(Employee emp) {
+		this.emp = emp;
+	}
+
+	public IdCard getEmpIdCard() {
+		return empIdCard;
+	}
+
+	public void setEmpIdCard(IdCard empIdCard) {
+		this.empIdCard = empIdCard;
+	}
+
+	public WorkStation getEmpWorkStation() {
+		return empWorkStation;
+	}
+
+	public void setEmpWorkStation(WorkStation empWorkStation) {
+		this.empWorkStation = empWorkStation;
+	}
+
+}
