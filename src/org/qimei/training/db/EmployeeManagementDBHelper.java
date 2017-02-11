2deea7fe07d21ff2f8f34ace8d6375e8db132eb8
qimei <qimei@192.168.0.10>
2017-02-11 17:02:43 +0800

    add update builder to update specific column and given value with empId condition

--- src/org/qimei/training/db/EmployeeManagementDBHelper.java
+++ src/org/qimei/training/db/EmployeeManagementDBHelper.java
@@ -7,6 +7,8 @@ import org.qimei.training.pojo.Employee;
 import org.qimei.training.pojo.IdCard;
 import org.qimei.training.pojo.WorkStation;
 
+import com.j256.ormlite.stmt.UpdateBuilder;
+
 public class EmployeeManagementDBHelper {
 
 	private static EmployeeManagementDBHelper empMgmtDBHelper = null;
@@ -21,8 +23,8 @@ public class EmployeeManagementDBHelper {
 		}
 		return empMgmtDBHelper;
 	}
-	
-	//collect new data for employee/idcard/workstation
+
+	// collect new data for employee/idcard/workstation
 	public int insertIntoEmps(Employee emp) throws SQLException {
 		int rowCount = EmployeeManagementDBManager.getInstance().getEmpDao().create(emp);
 
@@ -40,66 +42,80 @@ public class EmployeeManagementDBHelper {
 
 		return rowCount;
 	}
-	//update current data for employee/idcard/workstation
+
+	// update current data for employee/idcard/workstation
+	// ng - since this needs all the emp fields to be set - including the
+	// primary key or foreign key
 	public int updateExistingEmp(Employee emp) throws SQLException {
 		int rowCount = EmployeeManagementDBManager.getInstance().getEmpDao().update(emp);
-		
+
 		return rowCount;
 	}
-	
+
+	public int updateEmpById(int empId, String columnName, Object columnValue) throws SQLException {
+		
+		UpdateBuilder<Employee, Object> updateBuilder = EmployeeManagementDBManager.getInstance().getEmpDao().updateBuilder();
+		//update the given column name with the given column value
+		updateBuilder.updateColumnValue(columnName, columnValue);
+		//set condition to update by given rowId
+		updateBuilder.where().eq("empId", empId);
+		return updateBuilder.update();
+
+	}
+
 	public int updateExistingIdCard(IdCard empIdCard) throws SQLException {
 		int rowCount = EmployeeManagementDBManager.getInstance().getIdCardDao().update(empIdCard);
-		
+
 		return rowCount;
 	}
-	
+
 	public int updateExistingWorkStation(WorkStation empWorkStation) throws SQLException {
 		int rowCount = EmployeeManagementDBManager.getInstance().getWorkStationDao().update(empWorkStation);
-		
+
 		return rowCount;
 	}
-	
-	//delete current data for employee/idcard/workstation
+
+	// delete current data for employee/idcard/workstation
 	public int deleteExistingEmp(Employee emp) throws SQLException {
 		int rowCount = EmployeeManagementDBManager.getInstance().getEmpDao().delete(emp);
-		
+
 		return rowCount;
 	}
-	
+
 	public int deleteExistingIdCard(IdCard empIdCard) throws SQLException {
 		int rowCount = EmployeeManagementDBManager.getInstance().getIdCardDao().delete(empIdCard);
-		
+
 		return rowCount;
 	}
-	
+
 	public int deleteExistingWorkStation(WorkStation empWorkStation) throws SQLException {
 		int rowCount = EmployeeManagementDBManager.getInstance().getWorkStationDao().delete(empWorkStation);
-		
+
 		return rowCount;
 	}
-	
-	//get data from DB table
+
+	// get data from DB table
 	public List<Employee> getAllEmpsFromDB() throws SQLException {
 		return EmployeeManagementDBManager.getInstance().getEmpDao().queryForAll();
 	}
-	
-	public List<IdCard> getAllIdCardFromDB() throws SQLException{
+
+	public List<IdCard> getAllIdCardFromDB() throws SQLException {
 		return EmployeeManagementDBManager.getInstance().getIdCardDao().queryForAll();
 	}
-	
+
 	public List<WorkStation> getAllWorkStationFromDB() throws SQLException {
 		return EmployeeManagementDBManager.getInstance().getWorkStationDao().queryForAll();
 	}
-	
-	//get data from DB table by matching given ID
+
+	// get data from DB table by matching given ID
 	public List<Employee> getEmpById(int empId) throws SQLException {
 		return EmployeeManagementDBManager.getInstance().getEmpDao().queryForEq("empId", empId);
 	}
-	
+
 	public List<IdCard> getIdCardById(int empId) throws SQLException {
 		return EmployeeManagementDBManager.getInstance().getIdCardDao().queryForEq("employee_empId", empId);
 	}
-	
+
 	public List<WorkStation> getWorkStationById(int empId) throws SQLException {
 		return EmployeeManagementDBManager.getInstance().getWorkStationDao().queryForEq("employee_empId", empId);
 	}
