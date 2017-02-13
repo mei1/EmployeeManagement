9d86d67997b81cdd10274e27d08b826b857215c8
qimei <qimei@192.168.0.10>
2017-02-13 08:17:12 +0800

    svc for getempbyid

--- src/org/qimei/training/svcs/EmployeeManagementSvc.java
+++ src/org/qimei/training/svcs/EmployeeManagementSvc.java
@@ -4,8 +4,10 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.json.JSONException;
 import org.qimei.training.db.EmployeeManagementDBHelper;
 import org.qimei.training.pojo.Employee;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,12 @@ public class EmployeeManagementSvc {
 	public List<Employee> getAllEmp() throws SQLException {
 		logger.info("All emp request");
 		return EmployeeManagementDBHelper.getInstance().getAllEmpsFromDB();
+	}
+	
+	@RequestMapping (value = "empbyid", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	public List<Employee> getEmpById(@RequestBody Employee empIdObj) throws JSONException, SQLException {
+		logger.info("All emp request by ID - " + empIdObj);
+		return EmployeeManagementDBHelper.getInstance().getEmpById((int) empIdObj.getEmpId());
 	}
 
 }
